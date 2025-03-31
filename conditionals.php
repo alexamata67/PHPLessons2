dc902e183a09f9ef184261edec2baa67640818b3
alexamata67 <alexa.mata@students.jmcss.org>
2025-03-31 09:17:22 -0500

    yukum

---
+++ conditionals.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+   
+  <?php
+
+    $x = 10;
+    switch ($x){
+        case 10:
+            echo "The answer is 21";
+        break;
+        case 5:
+            echo "The answer is 22";
+        break;
+        case 6:
+            echo "The answer is 23";
+        break;
+        case 7:
+            echo "The answer is 24";
+        break;
+        default:
+        echo "There is no answer";
+    }
+
+
+  ?>
+  </body>
+</html>
