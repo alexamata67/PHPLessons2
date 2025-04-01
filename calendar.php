d9e31289149646ed6f67b17c0c20a59ca473dee2
alexamata67 <alexa.mata@students.jmcss.org>
2025-04-01 09:51:19 -0500

    ujkmuj

--- calendar.php
+++ calendar.php
@@ -1,14 +1,41 @@
 <!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
-    
+<html>
+<head>
+<meta charset="utf-8">
+<title>Title of the Document</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
-  
-  </body>
+<body>
+    
+  <?php
+    $dayofweek = date("w");
+ 
+
+ switch ($dayofweek){
+    case 1:
+        echo "It is Monday!";
+        break;
+    case 2:
+        echo "It is Tuesday!";
+        break; 
+    case 3:
+        echo "It is Wednesday!";
+        break;
+    case 4:
+        echo "It is Thursday!";
+        break;         
+    case 5:
+        echo "It is Friday!";
+         break;
+    case 6:
+        echo "It is Saturday!";
+        break; 
+    case 0:
+        echo "It is Sunday!";
+        break;  
+    }        
+?>
+
+</body>
 </html>
--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Title of the Document</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
    
   <form method="GET">
     <input type="text" name="class">
@@ -19,5 +19,5 @@
       //Finishes the sentence from class name 
       echo $name. " is my favorite class!";
   ?>
-  </body>
+</body>
 </html>
