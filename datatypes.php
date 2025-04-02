bc6f78c441481696710f42ec29c6312d86689b32
alexamata67 <alexa.mata@students.jmcss.org>
2025-04-02 09:27:37 -0500

    yes

--- datatypes.php
+++ datatypes.php
@@ -1,12 +1,38 @@
 <!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Title of the Document</title>
-<link rel="stylesheet" type="text/css" href="style.css">
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Index</a> 
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="operators.php">Operators</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="datatypes.php">Data Types</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="conditionals.php">Conditionals</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="calendar.php">Calendar</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+     
    
   <?php
    //string
